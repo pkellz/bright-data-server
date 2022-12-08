afec0b52fc309e7697cc2146639b078a768dafc0
pkellz <patrickscott2013@gmail.com>
2022-12-07 22:50:27 -0500

    Initial commit

---
+++ src/routes/api.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+
+// **** Init **** //
+
+const apiRouter = Router();
+
+
+// **** Setup auth routes **** //
+
+
+
+// **** Setup user routes **** //
+
+
+// Add userRouter
+// apiRouter.use(userRoutes.paths.basePath, userRouter);
+
+
+// **** Export default **** //
+
+export default apiRouter;
